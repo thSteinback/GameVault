e6766abc6fcd7ff4adae35633e79031bc9d0ddf4
thSteinback <thomas.hsteinback@gmail.com>
2026-06-14 23:43:22 -0300

    Atualização pra calculo de média

--- models/jogoModel.js
+++ models/jogoModel.js
@@ -2,7 +2,16 @@ const pool = require('../config/db');
 
 /* Catálogo público */
 exports.listarAtivos = () =>
-  pool.query('SELECT JOG_COD, JOG_NOME, JOG_DESC, JOG_IMG FROM jogos WHERE JOG_ATIVO = 1 ORDER BY JOG_DATA_CADASTRO DESC');
+  pool.query(
+    `SELECT j.JOG_COD, j.JOG_NOME, j.JOG_DESC, j.JOG_IMG,
+            ROUND(AVG(a.AVL_NOTA), 1) AS media,
+            COUNT(a.AVL_COD)          AS totalAval
+       FROM jogos j
+       LEFT JOIN avaliacoes a ON a.JOG_COD = j.JOG_COD
+      WHERE j.JOG_ATIVO = 1
+      GROUP BY j.JOG_COD
+      ORDER BY j.JOG_DATA_CADASTRO DESC`
+  );
 
 exports.buscarAtivoPorId = (id) =>
   pool.execute('SELECT JOG_COD, JOG_NOME, JOG_DESC, JOG_IMG FROM jogos WHERE JOG_COD = ? AND JOG_ATIVO = 1', [id]);
